4ff0446783902f8ea84d65adc79f9895294a0e21
Taha2525 <taha_25@hotmail.de>
2024-02-05 14:45:45 +0100

    Initial Commit

---
+++ TALPCARDS/TALPCARDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TALPCARDSApp.swift
+//  TALPCARDS
+//
+//  Created by Ahmet Ilgar on 05.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TALPCARDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
