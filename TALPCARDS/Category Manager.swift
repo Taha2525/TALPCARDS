d4ae00ec6dc11dc642d9359a03d49e6b6065d195
Taha2525 <taha_25@hotmail.de>
2024-02-06 17:02:58 +0100

    Actual status

---
+++ TALPCARDS/Category Manager.swift
@@ -0,0 +1,18 @@
+//
+//  Category Manager.swift
+//  TALPCARDS
+//
+//  Created by Taha Ilgar on 06.02.24.
+//
+
+import SwiftUI
+
+struct Category_Manager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Category_Manager()
+}
